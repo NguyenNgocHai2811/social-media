90df1dc669d9742ef34766410be766a05cc4817f
Nguyen Ngoc Hai <nguyenngochai2322@gmail.com>
2025-10-23 11:37:10 +0700

    udate main

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+
+const verifyToken = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(403).json({ message: 'A token is required for authentication' });
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.userId = decoded.id;
+    } catch (err) {
+        return res.status(401).json({ message: 'Invalid Token' });
+    }
+    return next();
+};
+
+module.exports = {
+    verifyToken,
+};
