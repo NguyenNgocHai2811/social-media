819d9e060ca8109fd68e36b8e675e851c7a95fb6
Nguyen Ngoc Hai <nguyenngochai2322@gmail.com>
2025-11-18 09:04:42 +0700

    delete bug

--- backend/src/routes/post.route.js
+++ backend/src/routes/post.route.js
@@ -9,7 +9,6 @@ router.get('/', authMiddleware.verifyToken, postController.getAllPosts);
 router.post('/',authMiddleware.verifyToken, uploadPostImage.single('image'), postController.createPost);
 
 // Nested route for comments on a specific post
-console.log("da vao post route");
 router.use('/:ma_bai_dang/comments', commentRoutes);
 
 module.exports = router;
